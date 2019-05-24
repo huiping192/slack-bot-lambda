0221ec9bc5e91ec3a7178937bd5a9aed0e4952c6
huiping.guo <huiping.guo@dena.jp>
2019-05-25 01:17:41 +0900

    first init

---
+++ slack-events-endpoint/bitriseService.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+
+func BuildIap(version string) {
+	sendRequest("tag",version,"iap-fastlane")
+
+	branch := "release/" + version
+	sendRequest("branch",branch,"iap-fastlane")
+}
+
+
+func BuildInhouse(version string) {
+	sendRequest("tag",version,"inhouse-fastlane")
+
+	branch := "release/" + version
+	sendRequest("branch",branch,"inhouse-fastlane")
+}
+
+
+func sendRequest(buildType string, buildTarget string, workflow string) {
+	token := os.Getenv("BITRISE_TOKEN")
+	appId := os.Getenv("BITRISE_APP_ID")
+
+	urlString := "https://app.bitrise.io/app/" + appId +"/build/start.json"
+
+	values := url.Values{}
+	values.Add("payload", "{\"hook_info\":{\"type\":\"bitrise\",\"build_trigger_token\":\"" + token + "\"},\"build_params\":{\""+buildType+"\":\""+ buildTarget + "\",\"workflow_id\":\""+ workflow +"\"},\"triggered_by\":\"curl\"}")
+
+	resp, _ := http.PostForm(urlString, values)
+
+
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response" + string(byteArray))
+}
---
+++ slack-events-endpoint/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/nlopes/slack"
+	"github.com/nlopes/slack/slackevents"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var token = os.Getenv("TOKEN")
+var verifyToken = os.Getenv("VERIFY_TOKEN")
+
+var api = slack.New(token)
+
+func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// retry の場合は無視
+	retryReason := request.Headers["X-Slack-Retry-Reason"]
+	if retryReason != "" {
+		return events.APIGatewayProxyResponse{
+			Body:       "",
+			StatusCode: http.StatusOK,
+		}, nil
+	}
+
+	body := request.Body
+	eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: verifyToken}))
+	if e != nil {
+		fmt.Println("[ERROR] ParseEvent error:" +  e.Error())
+		return events.APIGatewayProxyResponse{
+			Body:       "",
+			StatusCode: http.StatusInternalServerError,
+		}, nil
+	}
+
+	if eventsAPIEvent.Type == slackevents.URLVerification {
+		var r *slackevents.ChallengeResponse
+		err := json.Unmarshal([]byte(body), &r)
+		if err != nil {
+			fmt.Println("[ERROR] ParseEvent error:" +  e.Error())
+			return events.APIGatewayProxyResponse{
+				Body:       "",
+				StatusCode: http.StatusInternalServerError,
+			}, nil
+		}
+
+		return events.APIGatewayProxyResponse{
+			Body:      r.Challenge,
+			StatusCode: http.StatusOK,
+		}, nil
+	}
+	if eventsAPIEvent.Type == slackevents.CallbackEvent {
+		innerEvent := eventsAPIEvent.InnerEvent
+		switch ev := innerEvent.Data.(type) {
+		case *slackevents.AppMentionEvent:
+			handleMention(*ev)
+			return events.APIGatewayProxyResponse{
+				Body:       "",
+				StatusCode: http.StatusOK,
+			}, nil
+		}
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       "",
+		StatusCode: http.StatusOK,
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
+
+
+
+func handleMention(ev slackevents.AppMentionEvent) {
+	text := ev.Text
+
+	fmt.Println("mention words:" + text)
+
+	words := strings.Fields(text)
+
+	firstWord := words[1]
+
+	if firstWord == "help" {
+		handleHelp(ev)
+		return
+	}
+
+	if firstWord == "iap" {
+		handleIap(ev)
+		return
+	}
+
+	if firstWord == "inhouse" {
+		handleInhouse(ev)
+		return
+	}
+
+	sendMsg("なにいってるかわかりません。。。",ev.Channel)
+}
+
+
+func handleHelp(ev slackevents.AppMentionEvent) {
+	sendMsg("```コマンドサンプル: \n inhouse build: @ios_slack_bot inhouse v4.8.0 \n 課金build: @ios_slack_bot iap v4.8.0 ``",ev.Channel)
+}
+
+
+func handleIap(ev slackevents.AppMentionEvent) {
+	version := strings.Fields(ev.Text)[2]
+
+	BuildIap(version)
+
+	sendMsg("deploying iap build, version: " + version,ev.Channel)
+}
+
+func handleInhouse(ev slackevents.AppMentionEvent) {
+	version := strings.Fields(ev.Text)[2]
+
+	BuildInhouse(version)
+
+	sendMsg("deploying inhouse build, version: " + version,ev.Channel)
+}
+
+
+func sendMsg(mes string, channel string) {
+	_, _, _ = api.PostMessage(channel, slack.MsgOptionText(mes, false))
+}
