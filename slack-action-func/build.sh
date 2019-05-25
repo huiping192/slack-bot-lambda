76bdb55e536a5bb4f0b349711a7bb44314cff146
huiping.guo <huiping.guo@dena.jp>
2019-05-26 00:04:45 +0900

    cilからdeployできるようにする

--- slack-action-func/build.sh
+++ slack-action-func/build.sh
@@ -1,2 +1,4 @@
-GOOS=linux go build -o main
-zip main.zip main
+env GOOS=linux GOARCH=amd64 go build -o /tmp/main
+zip -j /tmp/main.zip /tmp/main
+aws lambda update-function-code --function-name slack-action-func \
+--zip-file fileb:///tmp/main.zip
--- slack-events-func/build.sh
+++ slack-events-func/build.sh
@@ -1,2 +1,4 @@
-GOOS=linux go build -o main
-zip main.zip main
+env GOOS=linux GOARCH=amd64 go build -o /tmp/main
+zip -j /tmp/main.zip /tmp/main
+aws lambda update-function-code --function-name slack-events-endpoint \
+--zip-file fileb:///tmp/main.zip
